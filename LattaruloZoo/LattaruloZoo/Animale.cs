9f2f09c6805fdef345b4df7d4f8d3dc6fe27fa4d
AndreaLattarulo <andrealattarulo08@gmail.com>
2025-10-22 11:40:35 +0200

    Visual Studio project files added

---
+++ LattaruloZoo/LattaruloZoo/Animale.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LattaruloZoo
+{
+    class Animale
+    {
+        public int Nome { get; set; }
+        public int Specie { get; set; }
+        public int Età { get; set; }
+        public int Peso { get; set; }
+        public int StatoDiSalute { get; set; }
+        public DateOnly DataArrivo { get; set; }
+        public int AnniAlloZoo { get; set; }
+
+      
+
+        private double _pesoIniziale;
+        private int _numeroControlliVeterinario;
+
+        private void CalcolaVariazionePesoPercentuale()
+        {
+
+        }
+        public virtual double CalcolaCiboDiarioKg()
+        {
+            return 0;
+        }
+        public virtual double CalcolaCostoGestioneMensile()
+        {
+            return 0;
+        }
+        public virtual double EmettiVerso()
+        {
+            return 0;
+        }
+        public void AggiornaPeso(double nuovoPeso)
+        {
+
+        }
+        public void EffettuaControlloVeterinario()
+        {
+            _numeroControlliVeterinario++;
+        }
+        public virtual double MostraInformazioni()
+        {
+            return 0;
+        }
+    }
+}
